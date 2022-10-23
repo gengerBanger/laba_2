8f2cddc30e1424dc55cbac9e22c67b707b0cd4a4
aleks <aleksandrsarajkin95@gmail.com>
2022-10-23 16:52:24 +0300

    pop

--- main.cpp
+++ main.cpp
@@ -13,7 +13,63 @@ void arr_out(int arr[]){ //функция вывода массива
     }
     cout << endl << endl;
 }
-void arr_sorting(int arr[],int end, int begin){ // функция сортировки массива
+
+void arr_sorting_bubble(int arr[], int n){
+    bool bub = true;
+    while(bub){
+        bub = false;
+        for (int i = 0; i < n ; i++){
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i],arr[i + 1]);
+                bub = true;
+            }
+        }
+        n--;
+    }
+
+}
+
+void arr_sorting_shaker(int arr[],int end, int begin){
+    bool shaker = true;
+    while(shaker){
+        shaker = false;
+        for (int i = begin; i < end; i++){
+            if (arr[i] > arr[i + 1]){
+                swap(arr[i],arr[i + 1]);
+                shaker = true;
+            }
+        }
+        end--;
+        for(int j = end ; j > begin; j--){
+            if (arr[j] < arr[j - 1]){
+                swap(arr[j], arr[j - 1]);
+                shaker = true;
+            }
+        }
+        begin++;
+    }
+}
+
+void arr_sorting_comb (int arr[], int end){
+    int s = end;
+    while (s >= 1){
+        for (int i = 0; i + s <= end; i++){
+            if (arr[i] > arr[i + s]) swap(arr[i], arr[i + s]);
+        }
+        s -= 1;
+    }
+
+}
+
+void arr_sorting_insert(int arr[], int n){
+    for (int i = 1; i <= n; i++){
+        for (int j = i; j > 0; j--){
+            if (arr[j] < arr[j - 1]) swap(arr[j], arr[j - 1]);
+        }
+    }
+}
+
+void arr_sorting_quick(int arr[],int end, int begin){ // функция сортировки массива
     int l = begin, r = end, mid = arr[(r + l) / 2];
     while (l < r){
         for(;arr[l] < mid; l++);
@@ -24,9 +80,10 @@ void arr_sorting(int arr[],int end, int begin){ // функция сортиро
             r--;
         }
     }
-    if (begin < r) arr_sorting(arr, r, begin);
-    if (l < end) arr_sorting(arr, end, l);
+    if (begin < r) arr_sorting_quick(arr, r, begin);
+    if (l < end) arr_sorting_quick(arr, end, l);
 }
+
 void max_min_value(int arr[]){ // функция для поиска максимального и минимального значения в массиве
     int max_value = -1000, min_value = 1000;
     auto start11 = chrono::high_resolution_clock::now();
@@ -68,13 +125,14 @@ void arr_exchange(int arr[], int a, int b ){ // функция для поиск
     arr_out(arr);
     cout << "Time to exchange = " << duration_4.count() << " s" <<'\n';
 }
+
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "cert-msc51-cpp"
 int main() {
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     cout.setf(ios_base::fixed);
     srand(time(nullptr));
-    int arr[100],arr_1[100], first_value, last_value;
+    int arr[100],arr_1[100], first_value, last_value, key_sort;
     for(int &i : arr){
         i = -99 + rand() % 199; // заполнение массива рандомными числами
     }
@@ -88,25 +146,85 @@ int main() {
 
     max_min_value(arr_1);
 
-    auto start_1 = chrono::high_resolution_clock::now();
-    arr_sorting(arr, 99 , 0);
-    auto finish_1 = chrono::high_resolution_clock::now();
-    chrono :: duration <float> duration = finish_1 - start_1;
-
-    SetConsoleTextAttribute(hConsole, 13);
-    cout << "Time spent sorting : " << duration.count() << " s" <<endl;
-    SetConsoleTextAttribute(hConsole, 15);
+    SetConsoleTextAttribute(hConsole, 14);
+    cout << "Choose sorting type :"<< '\n';
+    SetConsoleTextAttribute(hConsole, 10);
+    repeat:
+    cout <<"1) Bubble sort" <<'\n'<< "2) Shaker sort" <<'\n'<<"3) Comb sort" <<'\n'<<"4) Insert sort" <<'\n'<<"5) Quick sort" <<'\n'<< "enter sort number"<<'\n';
+    cin >> key_sort;
+    switch (key_sort) {
+        case 1:
+        {
+            auto start_1_1 = chrono::high_resolution_clock::now();
+            arr_sorting_bubble(arr, 99);
+            auto finish_1_1 = chrono::high_resolution_clock::now();
+            chrono :: duration <float> duration_1_1 = finish_1_1 - start_1_1;
+            SetConsoleTextAttribute(hConsole, 13);
+            cout << "Time spent sorting : " << duration_1_1.count() << " s" <<endl;
+            SetConsoleTextAttribute(hConsole, 15);
+            break;
+        }
+        case 2:
+        {
+            auto start_1_2 = chrono::high_resolution_clock::now();
+            arr_sorting_shaker(arr, 99, 0);
+            auto finish_1_2 = chrono::high_resolution_clock::now();
+            chrono :: duration <float> duration_1_2 = finish_1_2 - start_1_2;
+            SetConsoleTextAttribute(hConsole, 13);
+            cout << "Time spent sorting : " << duration_1_2.count() << " s" <<endl;
+            SetConsoleTextAttribute(hConsole, 15);
+            break;
+        }
+        case 3:
+        {
+            auto start_1_3 = chrono::high_resolution_clock::now();
+            arr_sorting_comb(arr, 99);
+            auto finish_1_3 = chrono::high_resolution_clock::now();
+            chrono :: duration <float> duration_1_3 = finish_1_3 - start_1_3;
+            SetConsoleTextAttribute(hConsole, 13);
+            cout << "Time spent sorting : " << duration_1_3.count() << " s" <<endl;
+            SetConsoleTextAttribute(hConsole, 15);
+            break;
+        }
+        case 4:
+        {
+            auto start_1_4 = chrono::high_resolution_clock::now();
+            arr_sorting_insert(arr, 99);
+            auto finish_1_4 = chrono::high_resolution_clock::now();
+            chrono :: duration <float> duration_1_4 = finish_1_4 - start_1_4;
+            SetConsoleTextAttribute(hConsole, 13);
+            cout << "Time spent sorting : " << duration_1_4.count() << " s" <<endl;
+            SetConsoleTextAttribute(hConsole, 15);
+            break;
+        }
+        case 5:
+        {
+            auto start_1_5 = chrono::high_resolution_clock::now();
+            arr_sorting_quick(arr, 99,0);
+            auto finish_1_5 = chrono::high_resolution_clock::now();
+            chrono :: duration <float> duration_1_5 = finish_1_5 - start_1_5;
+            SetConsoleTextAttribute(hConsole, 13);
+            cout << "Time spent sorting : " << duration_1_5.count() << " s" <<endl;
+            SetConsoleTextAttribute(hConsole, 15);
+            break;
+        }
+        default :
+            goto repeat;
+    }
 
     SetConsoleTextAttribute(hConsole, 10);
     cout <<'\t' <<"Sorted array :" << '\n';
     SetConsoleTextAttribute(hConsole, 15);
 
     arr_out(arr);
-
-    max_min_value(arr);
-
+    auto start_0 = chrono :: high_resolution_clock :: now();
     first_value = arr[0];
     last_value = arr[99];
+    auto finish_0 = chrono :: high_resolution_clock :: now();
+    chrono :: duration <float> duration_0 = finish_0 - start_0;
+    cout << "Minimum value in the array = "<< first_value << '\n' << "Maximum value in the array = " << last_value <<'\n';
+    cout << "Time to search for items in array = " << duration_0.count() << " s"<< '\n';
+
     mean(arr_1, first_value, last_value);
 
     int a, b, count = 0;
